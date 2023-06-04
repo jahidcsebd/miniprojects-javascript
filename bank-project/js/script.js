22cdcce044f7b3faca82fe63a6f1c7a5d1d4aad5
Hasan Jahid <dev.jahidcsebd@gmail.com>
2023-06-05 05:34:00 +0600

    added new function

--- bank-project/js/script.js
+++ bank-project/js/script.js
@@ -6,3 +6,41 @@ loginBtn.addEventListener("click", function () {
   const transactionArea = document.getElementById("transaction-area");
   transactionArea.style.display = "block";
 });
+
+//deposite button event handler
+const depositeBtn = document.getElementById("deposit-btn");
+depositeBtn.addEventListener("click", function () {
+  const depositNumber = getInputNumber("deposit-amount");
+
+  updateSpanText("currentDeposit", depositNumber);
+
+  updateSpanText("currentBallance", depositNumber);
+
+  document.getElementById("deposit-amount").value = "";
+});
+
+// Widthdraw button event handler
+const widthdrawBtn = document.getElementById("widthdraw-btn");
+widthdrawBtn.addEventListener("click", function() {
+  const widthdrawNumber = getInputNumber("widthdraw-amount");
+
+  updateSpanText("currentWidthdraw", widthdrawNumber);
+  updateSpanText("currentBallance", -1 * widthdrawNumber);
+
+  document.getElementById("widthdraw-amount").value = " ";
+});
+
+//deposit/widthdraw btn function
+function updateSpanText(id, addedNumber) {
+  const currentBallance = document.getElementById(id).innerText;
+  const currentNumber = parseFloat(currentBallance);
+  const totalBallance = addedNumber + currentNumber;
+  document.getElementById(id).innerText = totalBallance;
+
+};
+//get input
+function getInputNumber(id) {
+  const amount = document.getElementById(id).value;
+  const amountNumber = parseFloat(amount);
+  return amountNumber;
+}

af40a9820256db8121ad4140b4f636c2026d5ba5
Hasan Jahid <dev.jahidcsebd@gmail.com>
2023-06-04 20:37:12 +0600

    initial commit

---
+++ bank-project/js/script.js
@@ -0,0 +1,8 @@
+//login button event handler
+const loginBtn = document.getElementById("login");
+loginBtn.addEventListener("click", function () {
+  const loginArea = document.getElementById("login-area");
+  loginArea.style.display = "none";
+  const transactionArea = document.getElementById("transaction-area");
+  transactionArea.style.display = "block";
+});
